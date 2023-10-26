1a0798e3d746872ff927ac582ceff4de53ee86e9
Jayavarshini <121538052+Jayavarshini28@users.noreply.github.com>
2023-10-26 21:55:15 +0530

    Formatted Linear_search.c

--- Chapter-1/Linear_search.c
+++ Chapter-1/Linear_search.c
@@ -1,40 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
-int Linear_Search(int *arr,int element,int n)
-{
-    int i ,index = -1;
-    for(i = 0; i < n; i++) 
-    {
-        if(arr[i] == element)
-        {
-           index = i;
-           break;
-        }
-    }
-    return index ;
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+int Linear_Search(int *, int, int);
+
 int main()
 {
-    int n,i,element;
+    int n, i, element, *arr, result;
     printf("Enter size of array: ");
-    scanf("%d",&n);
-    int * arr = (int *) malloc(sizeof(int) * n);
-    for(i = 0;i<n;i++)
+    scanf("%d", &n);
+    arr = (int *)malloc(sizeof(int) * n); // Allocating n integers
+    for (i = 0; i < n; i++)
     {
         printf("Element :");
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
     printf("Enter Search Element: ");
-    scanf("%d",&element);
-    int result = Linear_Search(arr,element,n);
-    if(result != -1)
+    scanf("%d", &element);
+    result = Linear_Search(arr, element, n);
+    if (result != -1) // If search element  found
     {
-        printf("The search element is found at the %d th position in the array",result);
+        printf("The search element is found at the %d th position in the array", result);
     }
-    else
+    else // If search element not found
     {
         printf("The element search was not found");
     }
-    free(arr);
+    free(arr); // Deallocate memory
     return 0;
+}
+int Linear_Search(int *arr, int element, int n)
+{
+    int i, index = -1; // Initialize index at -1
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == element)
+        {
+            index = i; 
+            break;
+        }
+    }
+    return index; // Returns -1  if search element is not found
 }
