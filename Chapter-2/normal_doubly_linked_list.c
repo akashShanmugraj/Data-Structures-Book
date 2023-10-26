db84aa2b8b1e89da2b27d0b88255bacbc7c561ee
Hemanthkumar <83755355+Hemanth-kumar-05@users.noreply.github.com>
2023-10-26 21:46:48 +0530

    Formatted normal_doubly_linked_list.c

--- Chapter-2/normal_doubly_linked_list.c
+++ Chapter-2/normal_doubly_linked_list.c
@@ -2,29 +2,33 @@
 #include <stdlib.h>
 
 // Sturcture of Doubly Linked List
-struct Node {
+struct Node
+{
     int value;
     struct Node *prev, *next;
 };
 
-struct Node* getNode(int data);
-struct Node* insertAtBeginning(struct Node* start, int data);
-struct Node* insertAtEnd(struct Node* start, int data);
-struct Node* deleteAtBeginning(struct Node* start);
-struct Node* deleteAtEnd(struct Node* start);
-struct Node* searchNode(struct Node* start, int element);
-struct Node* insertAfterNodeX(struct Node* start, struct Node* NodeX, int data);
-struct Node* insertBeforeNodeX(struct Node* start, struct Node* NodeX, int data);
-struct Node* deleteAfterNodeX(struct Node* start, struct Node* NodeX);
-struct Node* deleteBeforeNodeX(struct Node* start, struct Node* NodeX);
-void displayLinkedList(struct Node* start);
+// Function declarations
+struct Node *getNode(int data);
+struct Node *insertAtBeginning(struct Node *start, int data);
+struct Node *insertAtEnd(struct Node *start, int data);
+struct Node *deleteAtBeginning(struct Node *start);
+struct Node *deleteAtEnd(struct Node *start);
+struct Node *searchNode(struct Node *start, int element);
+struct Node *insertAfterNodeX(struct Node *start, struct Node *NodeX, int data);
+struct Node *insertBeforeNodeX(struct Node *start, struct Node *NodeX, int data);
+struct Node *deleteAfterNodeX(struct Node *start, struct Node *NodeX);
+struct Node *deleteBeforeNodeX(struct Node *start, struct Node *NodeX);
+void displayLinkedList(struct Node *start);
 
 // Menu Driver Code
-int main() {
-    struct Node* start = NULL, *newNode, *NodeX;
+int main()
+{
+    struct Node *start = NULL, *newNode, *NodeX; // Assign head/start node to NULL
     int data, element, choice;
 
-    do {
+    do
+    {
         printf("\nDoubly Linked List Menu\n");
         printf("1. Add a node at beginning\n");
         printf("2. Add a node at end\n");
@@ -40,81 +44,89 @@ int main() {
 
         printf("Choose an option: ");
         scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                printf("Enter data to insert: ");
-                scanf("%d", &data);
-                start = insertAtBeginning(start, data);
-                break;
-            
-            case 2:
-                printf("Enter data to insert: ");
-                scanf("%d", &data);
-                start = insertAtEnd(start, data);
-                break;
-            
-            case 3:
-                start = deleteAtBeginning(start);
-                break;
+        switch (choice)
+        {
+        case 1:
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            start = insertAtBeginning(start, data);
+            break;
 
-            case 4:
-                start = deleteAtEnd(start);
-                break;
-            
-            case 5:
-                printf("Enter the value to be searched: ");
-                scanf("%d", &element);
-                NodeX = searchNode(start, element);
-                if (NodeX == NULL) printf("Value not found!\n");
-                else printf("Value is found at node with address (%p)\n", NodeX);
-                break;
-            
-            case 6:
-                printf("Enter the value of NodeX: ");
-                scanf("%d", &element);
-                NodeX = searchNode(start, element);
-                printf("Enter data to insert: ");
-                scanf("%d", &data);
-                start = insertAfterNodeX(start, NodeX, data);
-                break;
-            
-            case 7:
-                printf("Enter the value of NodeX: ");
-                scanf("%d", &element);
-                NodeX = searchNode(start, element);
-                printf("Enter data to insert: ");
-                scanf("%d", &data);
-                start = insertBeforeNodeX(start, NodeX, data);
-                break;
+        case 2:
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            start = insertAtEnd(start, data);
+            break;
 
-            case 8:
-                printf("Enter the value of NodeX: ");
-                scanf("%d", &element);
-                NodeX = searchNode(start, element);
-                start = deleteAfterNodeX(start, NodeX);
-                break;
-            
-            case 9:
-                printf("Enter the value of NodeX: ");
-                scanf("%d", &element);
-                NodeX = searchNode(start, element);
-                start = deleteBeforeNodeX(start, NodeX);
-                break;
+        case 3:
+            start = deleteAtBeginning(start);
+            break;
 
-            case 10:
-                displayLinkedList(start);
-                break;
-            
-            default:
-                printf("Terminating..\n");
-                exit(1);
+        case 4:
+            start = deleteAtEnd(start);
+            break;
+
+        case 5:
+            printf("Enter the value to be searched: ");
+            scanf("%d", &element);
+            NodeX = searchNode(start, element);
+            if (NodeX == NULL)
+                printf("Value not found!\n");
+            else
+                printf("Value is found at node with address (%p)\n", NodeX);
+            break;
+
+        case 6:
+            printf("Enter the value of NodeX: ");
+            scanf("%d", &element);
+            NodeX = searchNode(start, element);
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            start = insertAfterNodeX(start, NodeX, data);
+            break;
+
+        case 7:
+            printf("Enter the value of NodeX: ");
+            scanf("%d", &element);
+            NodeX = searchNode(start, element);
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            start = insertBeforeNodeX(start, NodeX, data);
+            break;
+
+        case 8:
+            printf("Enter the value of NodeX: ");
+            scanf("%d", &element);
+            NodeX = searchNode(start, element);
+            start = deleteAfterNodeX(start, NodeX);
+            break;
+
+        case 9:
+            printf("Enter the value of NodeX: ");
+            scanf("%d", &element);
+            NodeX = searchNode(start, element);
+            start = deleteBeforeNodeX(start, NodeX);
+            break;
+
+        case 10:
+            displayLinkedList(start);
+            break;
+
+        default:
+            printf("Terminating..\n");
+            // Deallocate pointers when exiting
+            free(start);
+            free(newNode);
+            free(NodeX);
+            exit(1);
         }
     } while (1);
 }
 
 // Creating a node in Doubly Linked List
-struct Node* getNode(int data) {
-    struct Node* newNode = (struct Node*) malloc (sizeof(struct Node));
+struct Node *getNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->value = data;
     newNode->prev = NULL; // Initialize previous address to NULL
     newNode->next = NULL; // Initialize next address to NULL
@@ -122,30 +134,39 @@ struct Node* getNode(int data) {
 }
 
 // Insertion in Doubly Linked List
-struct Node* insertAtBeginning(struct Node* start, int data) {
+// Insert node at start
+struct Node *insertAtBeginning(struct Node *start, int data)
+{
+    struct Node *newNode;
     // The list is empty
-    if (start == NULL) {
+    if (start == NULL)
+    {
         start = getNode(data);
         return start;
     }
     // The list is not empty
-    struct Node* newNode = getNode(data);
+    newNode = getNode(data);
     newNode->next = start;
     start->prev = newNode;
     start = newNode;
     return start;
 }
 
-struct Node* insertAtEnd(struct Node* start, int data) {
+// Insert node at end
+struct Node *insertAtEnd(struct Node *start, int data)
+{
+    struct Node *newNode, *current;
     // The list is empty
-    if (start == NULL) {
+    if (start == NULL)
+    {
         start = getNode(data);
         return start;
     }
     // The list is not empty
-    struct Node* newNode = getNode(data);
-    struct Node* current = start;
-    while (current->next != NULL) {
+    newNode = getNode(data);
+    current = start;
+    while (current->next != NULL)
+    {
         current = current->next;
     }
     current->next = newNode;
@@ -154,38 +175,49 @@ struct Node* insertAtEnd(struct Node* start, int data) {
 }
 
 // Deletion in Doubly Linked List
-struct Node* deleteAtBeginning(struct Node* start) {
+// Delete start node
+struct Node *deleteAtBeginning(struct Node *start)
+{
+    struct Node *temp;
     // The list is empty
-    if (start == NULL) {
+    if (start == NULL)
+    {
         printf("The list is empty.\n");
         return start;
     }
     // The list is not empty
-    struct Node* temp = start;
+    temp = start;
     start = start->next;
     start->prev = NULL;
     free(temp);
     return start;
 }
 
-struct Node* deleteAtEnd(struct Node* start) {
+// Delete end node
+struct Node *deleteAtEnd(struct Node *start)
+{
+    struct Node *current, *temp;
     // The list is empty
-    if (start == NULL) {
+    if (start == NULL)
+    {
         printf("The list is empty.\n");
         return start;
     }
     // The list is not empty
     // If list has only one element
-    if (start->next == NULL) {
+    if (start->next == NULL)
+    {
         free(start);
         return NULL;
     }
-    else {
-        struct Node* current = start;
-        while (current->next != NULL) {
+    else
+    {
+        current = start;
+        while (current->next != NULL)
+        {
             current = current->next;
         }
-        struct Node* temp = current;
+        temp = current;
         current->prev->next = NULL;
         free(temp);
         return start;
@@ -193,40 +225,51 @@ struct Node* deleteAtEnd(struct Node* start) {
 }
 
 // Search for element
-struct Node* searchNode(struct Node* start, int element) {
-    struct Node* current = start;
-    while (current != NULL) {
-        if (current->value == element) {
+struct Node *searchNode(struct Node *start, int element)
+{
+    struct Node *current = start;
+    // Traverse till end of linked list
+    while (current != NULL)
+    {
+        if (current->value == element)
+        {
             return current;
         }
         current = current->next;
     }
-    return NULL;
+    return NULL; // If node not found
 }
 
-// Insertion, Deletion after or before NodeX
-struct Node* insertAfterNodeX(struct Node* start, struct Node* NodeX, int data) {
-    struct Node* current = start;
-    int found = 0;
-    while (current != NULL) {
-        if (current == NodeX) {
+// Insertion, Deletion after or before Node X
+// Insert after Node X
+struct Node *insertAfterNodeX(struct Node *start, struct Node *NodeX, int data)
+{
+    struct Node *current = start, *newNode, *temp;
+    int found = 0; // To keep track of Node X
+    while (current != NULL)
+    {
+        if (current == NodeX)
+        {
             found = 1;
             break;
         }
         current = current->next;
     }
-    if (!found) {
+    // If NodeX not found in linked list
+    if (!found)
+    {
         printf("Node not found!\n");
         return start;
     }
-    
-    if (current->next == NULL) {
-        // The end node
+    // If NodeX points to end node
+    if (current->next == NULL)
+    {
         start = insertAtEnd(start, data);
     }
-    else {
-        struct Node* newNode = getNode(data);
-        struct Node* temp = current->next;
+    else
+    {
+        newNode = getNode(data);
+        temp = current->next;
         current->next = newNode;
         newNode->prev = current;
         newNode->next = temp;
@@ -235,28 +278,35 @@ struct Node* insertAfterNodeX(struct Node* start, struct Node* NodeX, int data) 
     return start;
 }
 
-struct Node* insertBeforeNodeX(struct Node* start, struct Node* NodeX, int data) {
-    struct Node* current = start;
-    int found = 0;
-    while (current != NULL) {
-        if (current == NodeX) {
+// Insert before Node X
+struct Node *insertBeforeNodeX(struct Node *start, struct Node *NodeX, int data)
+{
+    struct Node *current = start, *newNode, *temp;
+    int found = 0; // To keep track of Node X
+    while (current != NULL)
+    {
+        if (current == NodeX)
+        {
             found = 1;
             break;
         }
         current = current->next;
     }
-    if (!found) {
+    // If NodeX is not found in linked list
+    if (!found)
+    {
         printf("Node not found!\n");
         return start;
     }
-    
-    if (current->prev == NULL) {
-        // The start node
+    // If NodeX points to starting node
+    if (current->prev == NULL)
+    {
         start = insertAtBeginning(start, data);
     }
-    else {
-        struct Node* newNode = getNode(data);
-        struct Node* temp = current->prev;
+    else
+    {
+        newNode = getNode(data);
+        temp = current->prev;
         current->prev = newNode;
         newNode->next = current;
         newNode->prev = temp;
@@ -265,26 +315,34 @@ struct Node* insertBeforeNodeX(struct Node* start, struct Node* NodeX, int data)
     return start;
 }
 
-struct Node* deleteAfterNodeX(struct Node* start, struct Node* NodeX) {
-    struct Node* current = start;
-    int found = 0;
-    while (current != NULL) {
-        if (current == NodeX) {
+// Delete after Node X
+struct Node *deleteAfterNodeX(struct Node *start, struct Node *NodeX)
+{
+    struct Node *current = start, *temp;
+    int found = 0; // To keep track of Node X
+    while (current != NULL)
+    {
+        if (current == NodeX)
+        {
             found = 1;
             break;
         }
         current = current->next;
     }
-    if (!found || current->next == NULL) {
+    // If Node X is not found or no nodes are found after Node X
+    if (!found || current->next == NULL)
+    {
         printf("Node not found or no node to delete!\n");
         return start;
     }
-    if (current->next->next == NULL) {
-        // The last second node
+    // If NodeX points to last before node
+    if (current->next->next == NULL)
+    {
         start = deleteAtEnd(start);
-    } 
-    else {
-        struct Node* temp = current->next;
+    }
+    else
+    {
+        temp = current->next;
         current->next = current->next->next;
         current->next->prev = current;
         free(temp);
@@ -292,26 +350,34 @@ struct Node* deleteAfterNodeX(struct Node* start, struct Node* NodeX) {
     return start;
 }
 
-struct Node* deleteBeforeNodeX(struct Node* start, struct Node* NodeX) {
-    struct Node* current = start;
-    int found = 0;
-    while (current != NULL) {
-        if (current == NodeX) {
+// Delete before Node X
+struct Node *deleteBeforeNodeX(struct Node *start, struct Node *NodeX)
+{
+    struct Node *current = start, *temp;
+    int found = 0; // To keep track of Node X
+    while (current != NULL)
+    {
+        if (current == NodeX)
+        {
             found = 1;
             break;
         }
         current = current->next;
     }
-    if (!found || current->prev == NULL) {
+    // If Node X is not found or no nodes are found before Node X
+    if (!found || current->prev == NULL)
+    {
         printf("Node not found or no node to delete!\n");
         return start;
     }
-    if (current->prev->prev == NULL) {
-        // The last second node
+    // If Node X points to second node
+    if (current->prev->prev == NULL)
+    {
         start = deleteAtBeginning(start);
-    } 
-    else {
-        struct Node* temp = current->prev;
+    }
+    else
+    {
+        temp = current->prev;
         current->prev = current->prev->prev;
         current->prev->next = current;
         free(temp);
@@ -320,10 +386,12 @@ struct Node* deleteBeforeNodeX(struct Node* start, struct Node* NodeX) {
 }
 
 // Display Doubly Linked List
-void displayLinkedList(struct Node* start) {
-    struct Node* current = start;
+void displayLinkedList(struct Node *start)
+{
+    struct Node *current = start;
     printf("NULL -> ");
-    while (current != NULL) {
+    while (current != NULL)
+    {
         printf("%d -> ", current->value);
         current = current->next;
     }
