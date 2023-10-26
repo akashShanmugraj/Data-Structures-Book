f9b8d9c7157e920dee69ce97f058fce005878c52
Jayavarshini <121538052+Jayavarshini28@users.noreply.github.com>
2023-10-26 12:32:29 +0530

    Update Binary_search.c

--- Chapter-1/Binary_search.c
+++ Chapter-1/Binary_search.c
@@ -1,46 +1,46 @@
-#include<stdio.h>
-#include<stdlib.h>
-int binary(int *arr,int low,int high,int value)
-{
-    if(low>high)
-    {
-        printf("Element not found");
-        return -1;
-    }
-    int mid = (low+high)/2;
-    if( arr[mid] == value)
-    {
-        printf("Element found in the array");
-        return mid;
-    }
-    else if(arr[mid] > value)
-    {
-        binary(arr,low,mid-1,value);
-    }
-    else
-    {
-        binary(arr,mid+1,high,value);
+#include <stdio.h>
+#include <stdlib.h>
 
-    }
-    
-}
+int binarySearch(int *, int, int, int);
 
 int main()
 {
-    int n,value;
-    printf("Enter number of array elements:");
-    scanf("%d",&n);
-    int *arr =(int *)malloc(sizeof(int)*n);
-    for(int i=0;i<n;i++)
+    int n, value;
+    printf("Enter number of array elements: ");
+    scanf("%d", &n);
+    int *arr = (int *)malloc(sizeof(int) * n); // Allocating n integers
+    for (int i = 0; i < n; i++)
     {
-        printf("Enter elements %d " ,i);
-        scanf("%d",&arr[i]);
+        printf("Enter elements %d ", i);
+        scanf("%d", &arr[i]);
     }
-    printf("Enter search value:");
-    scanf("%d",&value);
-    int result =  binary(arr,0 ,n-1,value);
-    printf("the position: %d",result);
+    printf("Enter search value: ");
+    scanf("%d", &value);
+    int result = binarySearch(arr, 0, n - 1, value);
+    printf("The position: %d", result);
     free(arr);
     return 0;
-    
+}
+
+int binarySearch(int *arr, int low, int high, int value)
+{
+    if (low > high)
+    {
+        printf("Element not found\n");
+        return -1;
+    }
+    int mid = (low + high) / 2;
+    if (arr[mid] == value) // Target found at mid
+    {
+        printf("Element found in the array\n");
+        return mid;
+    }
+    else if (arr[mid] > value) // Target lesser than mid's value
+    {
+        binarySearch(arr, low, mid - 1, value);
+    }
+    else // Target greater than mid's value
+    {
+        binarySearch(arr, mid + 1, high, value);
+    }
 }
